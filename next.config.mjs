4f3dd86fac607d2dab2717de48ac600709776110
ahmar <ahmarcode@gmail.com>
2024-07-07 22:08:20 +0530

    FIXES

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: isProd ? '/NetSpeed/' : '',
+  basePath: isProd ? '/NetSpeed' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
